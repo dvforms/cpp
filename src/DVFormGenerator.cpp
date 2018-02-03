5a69dab065e10e7481cedea9f0f30ed4e091112b
David van Laatum <david@vanlaatum.id.au>
2018-02-03 12:38:31 +1030

    initial

---
+++ src/DVFormGenerator.cpp
@@ -0,0 +1,1 @@
+#include "DVFormGenerator.h"
---
+++ src/DVFormGenerator.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef DVMON_DVFORMGENERATOR_H
+#define DVMON_DVFORMGENERATOR_H
+
+class DVFormGenerator {
+};
+
+#endif //DVMON_DVFORMGENERATOR_H
