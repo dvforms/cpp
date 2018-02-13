02b0e9d389ff568085abf2525a3b9dd90eedc0eb
David van Laatum <david@vanlaatum.id.au>
2018-02-13 16:57:49 +1030

    initial

--- test/FormGeneratorTest.cpp
+++ test/FormGeneratorTest.cpp
@@ -1,18 +1,16 @@
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>    // IWYU pragma: keep
-#include "Forms.h"
-#include <json.h>           // for JSON, JSONDiffListenerImpl, operator<<
-#include <fstream>
-#include <memory>           // for __shared_ptr_access, shared_ptr, make_shared
+#include <gmock/gmock.h> // IWYU pragma: keep
+#include <gtest/gtest.h> //
+#include "Forms.h"       //
+#include <fstream>       //
+#include <json.h>        // for JSON, JSONDiffListenerImpl, operator<<
+#include <memory>        // for __shared_ptr_access, shared_ptr, make_shared
 
 using namespace testing;
 using namespace dv::forms;
 using namespace dv::json;
 
 class FormGeneratorTest : public ::testing::Test {
-  void SetUp() override {
-    RecordProperty( "COVERS", "FormGenerator.cpp:FormGenerator.h" );
-  }
+  void SetUp() override { RecordProperty( "COVERS", "FormGenerator.cpp:FormGenerator.h" ); }
 };
 
 TEST_F( FormGeneratorTest, Test ) {
@@ -24,14 +22,13 @@ TEST_F( FormGeneratorTest, Test ) {
   JSON expected;
   expected["$schema"] = "http://dvforms.org/v1#";
   expected["sections"]["ABC"]["order"] = 2;
-expected["sections"]["ABC"]["fields"] = {
-};
+  expected["sections"]["ABC"]["fields"] = {};
   expected["sections"]["Main"]["order"] = 1;
   expected["sections"]["Main"]["fields"]["test"]["order"] = 1;
   expected["sections"]["Main"]["fields"]["test"]["required"] = true;
   expected["sections"]["Main"]["fields"]["test"]["type"] = "text";
-//  expected["expressions"] = {};
-//  expected["properties"] = {};
+  //  expected["expressions"] = {};
+  //  expected["properties"] = {};
   auto schema = form->generateSchema();
 
   JSONDiffListenerImpl listener;
@@ -54,7 +51,5 @@ TEST_F( FormGeneratorTest, ParseTest ) {
   auto schema = form->generateSchema();
   JSONDiffListenerImpl listener;
   j.compare( schema, listener );
-EXPECT_EQ( j, schema
-) <<
-listener;
+  EXPECT_EQ( j, schema ) << listener;
 }
--- test/testmain.cpp
+++ test/testmain.cpp
@@ -1,13 +1,11 @@
-#include "gtest/gtest.h"
 #include "gmock/gmock.h"
-#include <stdlib.h>       // for getenv
+#include "gtest/gtest.h"
+#include <stdlib.h> // for getenv
 
 int main( int argc, char **argv ) {
   testing::InitGoogleMock( &argc, argv );
 
   int exit = RUN_ALL_TESTS();
-  if ( getenv( "IGNORE_EXIT_CODE" ) != nullptr ) {
-    return 0;
-  }
+  if ( getenv( "IGNORE_EXIT_CODE" ) != nullptr ) { return 0; }
   return exit;
 }
