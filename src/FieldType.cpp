04c29ee35e7991c9377ed1d1e1fb021341c85616
David van Laatum <david@vanlaatum.id.au>
2018-02-13 16:48:32 +1030

    initial

--- src/FieldType.cpp
+++ src/FieldType.cpp
@@ -3,21 +3,29 @@
 
 void dv::forms::to_json( json &json, dv::forms::FieldType type, const dv::json::JSONPath & /*path*/ ) {
   switch ( type ) {
-    case FieldType::TEXT: json = "text";
+    case FieldType::TEXT:
+      json = "text";
       break;
-    case FieldType::NUMBER: json = "number";
+    case FieldType::NUMBER:
+      json = "number";
       break;
-    case FieldType::EMAIL: json = "email";
+    case FieldType::EMAIL:
+      json = "email";
       break;
-    case FieldType::CURRENCY: json = "currency";
+    case FieldType::CURRENCY:
+      json = "currency";
       break;
-    case FieldType::STATIC: json = "static";
+    case FieldType::STATIC:
+      json = "static";
       break;
-    case FieldType::ENUM: json = "enum";
+    case FieldType::ENUM:
+      json = "enum";
       break;
-    case FieldType::MULTIPLE: json = "multiple";
+    case FieldType::MULTIPLE:
+      json = "multiple";
       break;
-    case FieldType::REPEATING: json = "repeating";
+    case FieldType::REPEATING:
+      json = "repeating";
       break;
   }
 }
